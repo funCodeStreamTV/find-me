7b0165bedca003b047365e1b4df08ee9d38b5e38
FunCodeStreamTV <funcodestreamtv@gmail.com>
2022-01-30 16:14:16 +0300

    FindMe 1 - Create spring boot application

---
+++ src/test/java/ru/aincorp/findme/FindmeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aincorp.findme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
